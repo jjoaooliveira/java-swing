df1aa261592f1d69ac55a7e2f8dabfdf251d32eb
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2024-08-06 20:20:25 -0400

    initial commit

---
+++ src/main/java/com/mycompany/poobd/Main.java
@@ -0,0 +1,15 @@
+package com.mycompany.poobd;
+
+import com.mycompany.poobd.Controller.CarroController;
+import com.mycompany.poobd.Database.CarroDAO;
+import com.mycompany.poobd.Gui.Frame;
+import com.mycompany.poobd.Usecase.CarroUseCase;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Frame frame = new Frame(new CarroController(new CarroUseCase(new CarroDAO())));
+        frame.main();
+    }
+}
