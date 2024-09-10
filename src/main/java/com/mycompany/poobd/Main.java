1ac4e96ed07d8f2ffb057960bef0c41dc76b5817
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2024-09-10 18:02:25 -0400

    refatorando main class

--- src/main/java/com/mycompany/poobd/Main.java
+++ src/main/java/com/mycompany/poobd/Main.java
@@ -3,12 +3,12 @@ package com.mycompany.poobd;
 import com.mycompany.poobd.Controller.CarroController;
 import com.mycompany.poobd.Database.CarroDAO;
 import com.mycompany.poobd.Gui.Frame;
-import com.mycompany.poobd.Usecase.CarroUseCase;
+import com.mycompany.poobd.Services.CarroService;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Frame frame = new Frame(new CarroController(new CarroUseCase(new CarroDAO())));
+    public static void main(String... args) {
+        Frame frame = new Frame(new CarroController(new CarroService(new CarroDAO())));
         frame.main();
     }
 }
