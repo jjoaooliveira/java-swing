912662fffdeb3dea1c8962d1f248a48ba0fde1e9
Joao Oliveira <88111035+jjoaooliveira@users.noreply.github.com>
2024-09-10 17:59:06 -0400

    refatorando controller

--- src/main/java/com/mycompany/poobd/Controller/IController.java
+++ src/main/java/com/mycompany/poobd/Controller/IController.java
@@ -3,9 +3,9 @@ package com.mycompany.poobd.Controller;
 import java.util.ArrayList;
 
 public interface IController {
-    boolean saveInputData(String[] rawInputData);
-    boolean updateInputData(String id, String[] rawInputData);
-    ArrayList<String[]> searchByModel(String model);
-    ArrayList<String[]> searchAll();
-    boolean delete(String id);
+    Response saveData(String model, String color, String year);
+    Response updateData(String id, String model, String color, String year);
+    Response searchByModel(String model);
+    Response searchAll();
+    Response delete(String id);
 }
